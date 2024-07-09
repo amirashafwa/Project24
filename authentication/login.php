d5ef4165ae3ef041ac81754d87794b5def1c040e
Amira Shafwa <96779841+amirashafwa@users.noreply.github.com>
2024-07-09 09:12:06 +0700

    Add files via upload
    update auth page

--- authentication/login.php
+++ authentication/login.php
@@ -33,9 +33,9 @@ if (isset($_POST['register'])) {
     if ($petugas_check > 0 || $user_check > 0) {
         echo '<script>alert("Username/Email sudah digunakan");</script>';
     } else {
-        $query = mysqli_query($koneksi, "INSERT INTO user (Username, Password, Email, NamaLengkap, Alamat) VALUES ('$username', '$password', '$email', '$namalengkap','$alamat')");
+        $query = mysqli_query($koneksi, "INSERT INTO user (Username, Password, Email, NamaLengkap, Alamat) VALUES ('$username', '$password', '$email', '$namalengkap', '$alamat')");
         if ($query) {
-            echo '<script>alert("Register Berhasil, Selamat Datang!"); location.href="../code/index.php"</script>';
+            echo '<script>alert("Register Berhasil, Silahkan Login!");</script>';
         } else {
             echo '<script>alert("Register Gagal!");</script>';
         }
@@ -43,49 +43,50 @@ if (isset($_POST['register'])) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" >
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Perpustakaan Digital</title>
-    <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
-    <link rel="stylesheet" href="../assets/css/login.css" />
+  <meta charset="UTF-8">
+  <title>Perpustakaan Digital</title>
+  <link rel="stylesheet" href="../assets/css/login.css">
+  <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
 </head>
 <body>
-    <section class="wrapper">
-        <div class="form signup">
-            <header>Register</header>
-            <form method="post">
-                <input type="text" placeholder="Nama Lengkap" name="namalengkap" required/>
-                <input type="text" placeholder="Username" name="username" required/>
-                <input type="email" placeholder="Email" name="email" required/>
-                <input type="password" placeholder="Password" name="password" required/>
-                <textarea name="alamat" rows="3" placeholder="Alamat" required></textarea>
-                <button type="submit" name="register">Submit</button>
-            </form>
-        </div>
-        <div class="form login">
-            <header>Login</header>
-            <form method="post">
-                <input type="text" placeholder="Username" name="username" required />
-                <input type="password" placeholder="Password" name="password" required />
-                <button type="submit" name="login">Submit</button>
-            </form>
-        </div>
+<!-- partial:index.partial.html -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Slide Navbar</title>
+	<link rel="stylesheet" type="text/css" href="slide navbar style.css">
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true">
 
-        <script>
-            const wrapper = document.querySelector(".wrapper"),
-                signupHeader = document.querySelector(".signup header"),
-                loginHeader = document.querySelector(".login header");
+			<div class="signup">
+				<form method="post">
+					<label for="chk" aria-hidden="true">Sign up</label>
+					<input autocomplete="off" type="text" placeholder="Nama Lengkap" name="namalengkap" required/>
+                    <input autocomplete="off" type="text" placeholder="Username" name="username" required/>
+                    <input autocomplete="off" type="email" placeholder="Email" name="email" required/>
+                    <input autocomplete="off" type="password" placeholder="Password" name="password" required/>
+                    <textarea name="alamat" placeholder="Alamat"></textarea>
+                    <button type="submit" name="register">Submit</button>
+				</form>
+			</div>
 
-            loginHeader.addEventListener("click", () => {
-                wrapper.classList.add("active");
-            });
-            signupHeader.addEventListener("click", () => {
-                wrapper.classList.remove("active");
-            });
-        </script>
-    </section>
+			<div class="login">
+				<form method="post">
+					<label for="chk" aria-hidden="true">Login</label>
+					<input autocomplete="off" type="text" name="username" placeholder="Username" required="">
+					<input autocomplete="off" type="password" name="password" placeholder="Password" required="">
+					<button type="submit" name="login">Login</button>
+				</form>
+			</div>
+	</div>
+</body>
+</html>
+<!-- partial -->
+  
 </body>
 </html>
